a06de3a8b6111af6464ffb3492bdf50f9935158c
guillaume gaillard <gaillard.guillaum@gmail.com>
2021-10-07 19:51:03 +0200

    test

--- javascript/dependances/getJsonData.js
+++ javascript/dependances/getJsonData.js
@@ -1,9 +1,10 @@
 
 export const getJsonData = async() => {
-      const url = "../data/data.json"
+      const url = "../data/API/data.json"
       const jsonData = await fetch(url)
       const data = await jsonData.json()
-
+     
+      console.log(data);
       const photographData = [...data.photographers];
       const mediaData = [...data.media];
 
