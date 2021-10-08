807bb98db4fc114b244867f326b186fcca622dd3
guillaume gaillard <gaillard.guillaum@gmail.com>
2021-10-08 08:12:27 +0200

    test

--- javascript/dependances/getJsonData.js
+++ javascript/dependances/getJsonData.js
@@ -1,7 +1,10 @@
 
 export const getJsonData = async() => {
       const url = "../data/API/data.json"
-      const jsonData = await fetch(url)
+      const jsonData = await fetch(url , {headersheaders:{
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      }})
       const data = await jsonData.json()
      
       console.log(data);
